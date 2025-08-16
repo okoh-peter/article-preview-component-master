ac2e1a3c142954af3e53c4e93b98f03e7e8835ee
okoh-peter <okohpeter13@gmail.com>
2025-08-16 09:12:23 +0100

    Updated

--- java.js
+++ java.js
@@ -6,8 +6,19 @@ const btn = document.getElementById('sharebtn');
 const footers = document.querySelector('footer');
 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function(e){
+     e.stopPropagation();
     footers.style.display = 'block';
 
     console.log('click')
 })
+
+
+
+
+document.querySelector('body').addEventListener('click', function(){
+   footers.style.display = 'none';
+
+    console.log('click remove')
+})
+
